fa8a8b2c3b00c276b5c6d2bc5708327f771df350
David Cao <dccao@teco.com.ar>
2021-01-24 09:58:00 -0300

    First working version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
+import { SimpleGrid, Text, Input } from "@chakra-ui/react";
+import React, { useState } from "react";
+import "./App.css";
 
+let coso = new Array(25).fill("")
 function App() {
+  let [values, setValue] = useState({ column: 5, row: 5, cards: coso });
+  let [cards, setCards] = useState(coso);
+
+  const handleGridInputChange = (event) => {
+    setValue({
+      ...values,
+      [event.target.name]: parseInt(event.target.value, 10),
+    });
+  };
+
+  const handleInputChange = (event) => {
+    let newCardsArray = [...cards];
+    newCardsArray[event.target.name] = event.target.value;
+  };
+
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+      <Text fontSize='4em'>BINGO</Text>
+      
+
+        <Center>
+          <SimpleGrid columns={values.column}>
+            {values.cards
+              .slice(0, values.row * values.column)
+              .map((item, index) => (
+                <Input
+                  w="160px"
+                  h="160px"
+                  border="2px"
+                  borderColor="black.200"
+                  borderRadius="0"
+                  name={index}
+                  onChange={handleInputChange}
+                />
+              ))}
+          </SimpleGrid>
+        </Center>
+      </div>
+    </ChakraProvider>
   );
 }
 
